1d157073a86c362e73d466ded87d6e4fc73f1d9d
Dell <Dell@DELL>
2024-01-05 13:12:21 +0530

    first commit

---
+++ actitime/src/main/java/com/atctitime/generic/Listener.java
@@ -0,0 +1,5 @@
+package com.atctitime.generic;
+
+public class Listener {
+
+}
---
+++ actitime/src/test/java/com/actitime/TestScripts/CreateCustomerModule.java
@@ -0,0 +1,13 @@
+package com.actitime.TestScripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomerModule {
+	@Test
+	public void CreatCustomer() {
+		Reporter.log("creatCustomer");
+		
+	}
+
+}
